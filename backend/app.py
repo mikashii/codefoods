25badda310168ff5b9751c79e6ca706d1ba8cbf8
mikashii <carolina.portas@outlook.com>
2021-02-27 22:52:32 -0300

    add routes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, escape, request
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@app.route("/teste")
+def teste():
+    return "<h1>Sai dai doido</h1>" 
